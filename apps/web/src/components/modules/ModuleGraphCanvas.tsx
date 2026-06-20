6cd15941fc5fbbbc3a8ab42d8f3ed3c26a2983f7
Frank <wasutha1449@gmail.com>
2026-06-20 14:14:54 +0700

    fix: use Space+left-drag to pan instead of middle-click

--- apps/web/src/components/modules/ModuleGraphCanvas.tsx
+++ apps/web/src/components/modules/ModuleGraphCanvas.tsx
@@ -170,7 +170,7 @@ export function ModuleGraphCanvas({ modules, connections, projectId }: ModuleGra
             onPaneContextMenu={onPaneContextMenu}
             deleteKeyCode={["Delete", "Backspace"]}
             selectionOnDrag
-            panOnDrag={[1]}
+            panActivationKeyCode="Space"
             fitView
             attributionPosition="bottom-left"
             className="bg-neutral-950"
